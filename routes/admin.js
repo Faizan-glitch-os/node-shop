99fad51b24204b6ad95bd2ebee109aa2406ad770
Faizan Ahmad <faizanahmad2582001@gmail.com>
2025-07-09 16:19:04 +0500

    html file sent

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const adminRouter = express.Router();
 
 adminRouter.route("/add-products").get((req, res, next) => {
-  res.send(
-    "<form action='/admin/products' method='POST'><input type='text' name='input' /><button type='submit' >Add User </button></form>"
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 adminRouter.route("/products").post((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const shopRouter = express.Router();
 
 shopRouter.route("/").get((req, res, next) => {
-  res.send("<h1> Homepage </h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = shopRouter;
